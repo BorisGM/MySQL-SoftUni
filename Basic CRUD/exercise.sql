10f3168e65113dee19ea8764133118ab68f62db9
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-10-07 00:00:33 +0300

    Initial commit

---
+++ Basic CRUD/exercise.sql
@@ -0,0 +1,13 @@
+-- 01. Find All Information About Departments
+SELECT * FROM `departments`
+ORDER BY `department_id`;
+
+-- 02. Find all Department Names
+SELECT `name` FROM `departments`
+ORDER BY `department_id`;
+
+-- 03. Find Salary of Each Employee
+SELECT `first_name`, `last_name`, `salary`
+FROM `employees`
+ORDER BY `employee_id`;
+
