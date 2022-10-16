598da17c4ad477f9dfedf63666d8893aaa73fd2b
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-10-16 16:37:10 +0300

    ex

--- Data Aggregation/exercise.sql
+++ Data Aggregation/exercise.sql
@@ -94,3 +94,97 @@ WHERE
     `deposit_group` = 'Troll Chest'
 GROUP BY `first_letter`
 ORDER BY `first_letter`;
+
+-- 11. Average Interest
+SELECT
+    `deposit_group`,
+    `is_deposit_expired`,
+    AVG(`deposit_interest`) AS `deposit_interest`
+FROM
+    `wizzard_deposits`
+WHERE
+        `deposit_start_date` > '1985-01-01'
+GROUP BY `deposit_group` , `is_deposit_expired`
+ORDER BY `deposit_group` DESC , `is_deposit_expired` ASC;
+
+-- 12. Employees Minimum Salaries
+SELECT
+    `department_id`, MIN(`salary`) AS `minimum_salary`
+FROM
+    `employees`
+WHERE
+        `department_id` IN (2 , 5, 7)
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+-- 13. Employees Average Salaries
+SELECT
+    `department_id`,
+    CASE
+        WHEN `department_id` = 1 THEN AVG(`salary`) + 5000
+        ELSE AVG(`salary`)
+        END AS `avg_salary`
+FROM
+    `employees`
+WHERE
+        `salary` > 30000 AND `manager_id` != 42
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+-- 14. Employees Maximum Salaries
+SELECT
+    `department_id`, MAX(`salary`) AS `max_salary`
+FROM
+    `employees`
+GROUP BY `department_id`
+HAVING NOT `max_salary` BETWEEN 30000 AND 70000
+ORDER BY `department_id` ASC;
+
+-- 15. Employees Count Salaries
+SELECT
+    COUNT(`salary`) AS `count`
+FROM
+    `employees`
+WHERE
+    `manager_id` IS NULL;
+
+-- 16. 3rd Highest Salary*
+SELECT
+    `department_id`,
+    (SELECT DISTINCT
+         `salary`
+     FROM
+         `employees` AS `e`
+     WHERE
+             `e`.`department_id` = `employees`.`department_id`
+     ORDER BY `salary` DESC
+        LIMIT 1 OFFSET 2) AS `third_highest_salary`
+FROM
+    `employees`
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
+
+-- 17. Salary Challenge**
+SELECT
+    `first_name`, `last_name`, `department_id`
+FROM
+    `employees`
+WHERE
+        `salary` > (SELECT
+                        AVG(`salary`) AS `avg`
+                    FROM
+                        `employees` AS `e`
+                    WHERE
+                            `e`.`department_id` = `employees`.`department_id`)
+ORDER BY `department_id` ASC , `employee_id` ASC
+    LIMIT 10;
+
+-- 18. Departments Total Salaries
+SELECT
+    `department_id`,
+    SUM(`salary`) AS `total_salary`
+FROM
+    `employees`
+GROUP BY `department_id`
+ORDER BY `department_id`;
