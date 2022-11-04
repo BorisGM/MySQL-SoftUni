b7575f915eb1d7f7dda5b82975c99cb318bed952
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-11-04 20:38:30 +0200

    ex

--- Table Relations/lab.sql
+++ Table Relations/lab.sql
@@ -54,3 +54,38 @@ CREATE TABLE `peaks`
     ON DELETE CASCADE
 );
 
+-- 5. Project Management DB*
+CREATE DATABASE `company`;
+USE `company`;
+
+CREATE TABLE `clients`
+(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `client_name` VARCHAR(100)
+);
+
+CREATE TABLE `projects`
+(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `project_lead_id` INT(11),
+    `client_id` INT,
+    CONSTRAINT `fk_projects_clients`
+        FOREIGN KEY(`client_id`)
+            REFERENCES `clients`(`id`)
+);
+
+CREATE TABLE `employees`
+(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(30),
+    `last_name` VARCHAR(30),
+    `project_id` INT(11),
+    CONSTRAINT `fk_employees_projects`
+        FOREIGN KEY(`project_id`)
+            REFERENCES `projects`(`id`)
+);
+
+ALTER TABLE `projects`
+    ADD CONSTRAINT `fk_projects_employees`
+        FOREIGN KEY(`project_lead_id`)
+            REFERENCES `employees`(`id`);
